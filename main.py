66aa5efedd3bf56cb700a48948400ee640fc5c97
Brandon <73324449+BrandonW-ERAU@users.noreply.github.com>
2020-11-08 13:51:24 -0700

    Beta Version. Still needs docstrings and better variable names.

--- main.py
+++ main.py
@@ -6,26 +6,23 @@ __author__ = "Brandon Harris Willison"
 __email__ = "willisob@my.erau.edu"
 __version__ = "1.0"
 
-# noinspection PyUnresolvedReferences
 from os.path import getsize, join
 from time import time
-
-# noinspection PyUnresolvedReferences
 from p1utils import all_files, compare
 
 
 def search(file_list):
-    """Looking for duplicate files in the provided list of files
-    :returns a list of lists, where each list contains files with the same content
+    lol = []
+    while 0 < len(file_list):
+        dups = list(filter(lambda x: compare(file_list[0], x), file_list))
+        if 1 < len(dups):
+            lol.append(dups)
+        file_list = list(filter(lambda x: not compare(file_list[0], x), file_list))
+    return lol
 
-    Basic search strategy goes like this:
-    - until the provided list is empty.
-    - remove the 1st item from the provided file_list
-    - search for its duplicates in the remaining list and put the item and all its duplicates into a new list
-    - if that new list has more than one item (i.e. we did find duplicates) save the list in the list of lists
-    As a result we have a list, each item of that list is a list,
-    each of those lists contains files that have the same content
-    """
+def faster_search(file_list):
+    file_sizes = list(map(getsize, file_list))
+    file_list = list(filter(lambda x: 1 < file_sizes.count(getsize(x)), file_list))
     lol = []
     while 0 < len(file_list):  # problem gets smaller with every iteration, since items get removed
         dups = [file_list.pop(0)]
@@ -35,72 +32,31 @@ def search(file_list):
         if 1 < len(dups):
             lol.append(dups)
     return lol
-
-
-def faster_search(file_list):
-    """Looking for duplicate files in the provided list of files
-    :returns a list of lists, where each list contains files with the same content
-
-    Here's an idea: executing the compare() function seems to take a lot of time.
-    Therefore, let's optimize and try to call it a little less often.
-    """
-    lol = []
-    while 0 < len(file_list):
-        dups = list(filter(lambda x: compare(file_list[0], x), file_list))
-        if 1 < len(dups):
-            lol.append(dups)
-        file_list = list(filter(lambda x: not compare(file_list[0], x), file_list))
-    lol.sort(reverse=True)
-    return lol
-
-
-
 def report(lol):
-    """ Prints a report
-    :param lol: list of lists (each containing files with equal content)
-    :return: None
-    Prints a report:
-    - longest list, i.e. the files with the most duplicates
-    - list where the items require the largest amount or disk-space
-    """
+
+    # Most Copies
     lol.sort(reverse=False)
     m = max(lol, key=lambda x: len(x))
-
-    # dups which have the most characters .. again considering one origianl
-
 
     print("== == Duplicate File Finder Report == ==")
     print(f"The File with the most duplicates is:\n {m[0]}")
     print(f"Here are its {len(m)-1} copies: ")
     for file in m[1:]:
         print(file)
-    # if .... :
-    #
-    # else:
-    #     print("No duplicates found")
+
+    # Most Space
+    s = max(lol, key=lambda x: len(x) * getsize(x[0]))
+    print(f"The file taking up the most disk space({(len(s) - 1) * getsize(m[0])}) is: \n {s[0]}")
+    print(f"Here are its {len(s) - 1} copies: ")
+    for file in s[1:]:
+        print(file)
 
 
 if __name__ == '__main__':
-    path = join(".", 'images')
-
-    # measure how long the search and reporting takes:
     t0 = time()
-    report(search(all_files("/Users/cflock/Desktop/images")))
+    report(search(all_files("C:/Users/Brandon/Desktop/images")))
     print(f"Runtime: {time() - t0:.2f} seconds")
-
     print("\n\n .. and now w/ a faster search implementation:")
-
-    # measure how long the search and reporting takes:
     t0 = time()
-    report(faster_search(all_files("/Users/cflock/Desktop/images")))
+    report(faster_search(all_files("C:/Users/Brandon/Desktop/images")))
     print(f"Runtime: {time() - t0:.2f} seconds")
-
-
-
-
-
-
-
-
-
-
